19105fc0c859bc1eba708bacd225cb0c214caf18
Kody Low <kodylow7@gmail.com>
2022-04-17 10:53:49 -0700

    switched enitrely to vector draining

--- src/tx_structs/bitcoin_tx.rs
+++ src/tx_structs/bitcoin_tx.rs
@@ -15,25 +15,17 @@ impl BitcoinTX {
     pub fn new(hex_string: String) -> Self {
         let mut tx_bytes = hex::decode(hex_string).unwrap();
 
-        let mut version: [u8; 4] = [0; 4];
-        version.clone_from_slice(tx_bytes.drain(..4).as_slice());
-        let version = u32::from_le_bytes(version[..].try_into().unwrap());
+        let version = u32::from_le_bytes(tx_bytes.drain(..4).as_slice()[0..4].try_into().unwrap());
 
-        let (input_count, compact_field_len) = parsers::compact_field(tx_bytes.to_vec());
-        tx_bytes.drain(..compact_field_len);
+        let input_count = parsers::compact_field(&mut tx_bytes);
 
-        let (input_bytes_count, inputs) = parsers::tx_inputs(input_count, tx_bytes.to_vec());
-        tx_bytes.drain(..input_bytes_count);
+        let inputs = parsers::tx_inputs(input_count, &mut tx_bytes);
 
-        let (output_count, compact_field_len) = parsers::compact_field(tx_bytes.to_vec());
-        tx_bytes.drain(..compact_field_len);
+        let output_count = parsers::compact_field(&mut tx_bytes);
 
-        let (output_bytes_count, outputs) = parsers::tx_outputs(output_count, tx_bytes.to_vec());
-        tx_bytes.drain(..output_bytes_count);
+        let outputs = parsers::tx_outputs(output_count, &mut tx_bytes);
 
-        let mut locktime: [u8; 4] = [0; 4];
-        locktime.clone_from_slice(tx_bytes.drain(..4).as_slice());
-        let locktime = u32::from_le_bytes(locktime[..].try_into().unwrap());
+        let locktime = u32::from_le_bytes(tx_bytes.drain(..4).as_slice()[0..4].try_into().unwrap());
 
         Self {
             version,
--- src/tx_structs/parsers.rs
+++ src/tx_structs/parsers.rs
@@ -1,57 +1,40 @@
 use crate::tx_structs::{tx_input::TXInput, tx_output::TXOutput};
-use vecshard::{ShardExt, VecShard};
 
-pub fn compact_field(byte_vec: Vec<u8>) -> (usize, usize) {
-    let (size, byte_vec): (VecShard<u8>, VecShard<u8>) = byte_vec.split_inplace_at(1);
-
-    let size: usize = usize::try_from(size[0]).unwrap();
+pub fn compact_field(byte_vec: &mut Vec<u8>) -> usize {
+    let size: usize = usize::try_from(byte_vec[0]).unwrap();
+    byte_vec.drain(..1);
 
     if size <= 252 {
-        (size, 1)
+        size
     } else {
         let len_actual_size_bytes = size - 252;
         let actual_size_bytes = usize::pow(2, u32::try_from(len_actual_size_bytes).unwrap());
-        let size = usize::from_be_bytes(byte_vec[1..actual_size_bytes + 1].try_into().unwrap());
-        (size, len_actual_size_bytes + 1)
+        let size: Vec<u8> = byte_vec.drain(..actual_size_bytes).collect();
+        usize::from_le_bytes(size.as_slice().try_into().unwrap())
     }
 }
 
-pub fn tx_inputs(input_count: usize, byte_vec: Vec<u8>) -> (usize, Vec<TXInput>) {
+pub fn tx_inputs(input_count: usize, byte_vec: &mut Vec<u8>) -> Vec<TXInput> {
     let mut inputs: Vec<TXInput> = Vec::new();
-
-    let mut input_bytes_offset: usize = 0;
 
     let mut i = 0;
 
     while i < input_count {
-        let loc_byte_vec = byte_vec.clone();
-
-        let mut txid: [u8; 32] = [0; 32];
-        txid.clone_from_slice(&loc_byte_vec[input_bytes_offset..(input_bytes_offset + 32)]);
+        let mut txid: Vec<u8> = byte_vec.drain(..32).collect();
         txid.reverse();
-        let txid: String = hex::encode(txid);
-        input_bytes_offset += 32;
+        let txid: String = hex::encode(txid).clone();
 
-        let mut vout: [u8; 4] = [0; 4];
-        vout.clone_from_slice(&loc_byte_vec[input_bytes_offset..(input_bytes_offset + 4)]);
-        let vout: u32 = u32::from_le_bytes(vout[..].try_into().unwrap());
-        input_bytes_offset += 4;
+        let vout: u32 =
+            u32::from_le_bytes(byte_vec.drain(..4).as_slice()[0..4].try_into().unwrap());
 
-        let (script_sig_size, compact_field_len) =
-            compact_field(loc_byte_vec[input_bytes_offset..].to_vec());
+        let script_sig_size = compact_field(byte_vec);
 
-        input_bytes_offset += compact_field_len;
-
-        let script_sig: Vec<u8> =
-            loc_byte_vec[input_bytes_offset..input_bytes_offset + script_sig_size].to_vec();
+        let script_sig: Vec<u8> = byte_vec.drain(..script_sig_size).collect();
         let script_sig: String = hex::encode(script_sig);
 
-        input_bytes_offset += script_sig_size;
-
-        let mut sequence: [u8; 4] = [0; 4];
-        sequence.clone_from_slice(&loc_byte_vec[input_bytes_offset..(input_bytes_offset + 4)]);
+        let mut sequence: Vec<u8> = byte_vec.drain(..4).collect();
+        sequence.reverse();
         let sequence: String = hex::encode(sequence);
-        input_bytes_offset += 4;
 
         let inp = TXInput {
             txid,
@@ -65,34 +48,22 @@ pub fn tx_inputs(input_count: usize, byte_vec: Vec<u8>) -> (usize, Vec<TXInput>)
         inputs.push(inp);
     }
 
-    (input_bytes_offset, inputs)
+    inputs
 }
 
-pub fn tx_outputs(output_count: usize, byte_vec: Vec<u8>) -> (usize, Vec<TXOutput>) {
+pub fn tx_outputs(output_count: usize, byte_vec: &mut Vec<u8>) -> Vec<TXOutput> {
     let mut outputs: Vec<TXOutput> = Vec::new();
-
-    let mut output_bytes_offset: usize = 0;
 
     let mut i: usize = 0;
 
     while i < output_count {
-        let loc_byte_vec = byte_vec.clone();
+        let amount: u64 =
+            u64::from_le_bytes(byte_vec.drain(..8).as_slice()[0..8].try_into().unwrap());
 
-        let mut amount: [u8; 8] = [0; 8];
-        amount.clone_from_slice(&loc_byte_vec[output_bytes_offset..(output_bytes_offset + 8)]);
-        let amount: u64 = u64::from_le_bytes(amount);
-        output_bytes_offset += 8;
+        let script_pubkey_size = compact_field(byte_vec);
 
-        let (script_pubkey_size, compact_field_len) =
-            compact_field(loc_byte_vec[output_bytes_offset..].to_vec());
-
-        output_bytes_offset += compact_field_len;
-
-        let script_pubkey: Vec<u8> =
-            loc_byte_vec[output_bytes_offset..output_bytes_offset + script_pubkey_size].to_vec();
+        let script_pubkey: Vec<u8> = byte_vec.drain(..script_pubkey_size).collect();
         let script_pubkey: String = hex::encode(script_pubkey);
-
-        output_bytes_offset += script_pubkey_size;
 
         let out = TXOutput {
             amount,
@@ -104,5 +75,5 @@ pub fn tx_outputs(output_count: usize, byte_vec: Vec<u8>) -> (usize, Vec<TXOutpu
         i += 1;
     }
 
-    (output_bytes_offset, outputs)
+    outputs
 }
